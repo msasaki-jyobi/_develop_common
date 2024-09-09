62ee58a7371648cef606fbd791360ce67f898f36
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-09-09 13:21:05 +0900

    Update dl_shooter.bat完成

--- dl_develop_shooter.bat
+++ dl_develop_shooter.bat
@@ -2,33 +2,22 @@
 :: バッチファイルのディレクトリを取得
 set scriptdir=%~dp0
 
-:: リポジトリのクローン先を指定
-set target_dir=%scriptdir%..\
-
-:: 一時クローン先（_tempフォルダ）
-set temp_dir=%target_dir%_temp
+:: クローン先のディレクトリを指定
+set target_dir=%scriptdir%..\_develop_shooter
 
 :: GitリポジトリのURL
-set repo_url=https://github.com/msasaki-jyobi/_shooter_develop.git
+set repo_url=https://github.com/msasaki-jyobi/_develop_shooter.git
 
-:: 一時クローン先がすでに存在する場合、削除
-if exist "%temp_dir%" (
-    echo Removing existing temporary folder...
-    rmdir /S /Q "%temp_dir%"
+:: クローン先がすでに存在する場合、削除
+if exist "%target_dir%" (
+    echo Removing existing _develop_shooter folder...
+    rmdir /S /Q "%target_dir%"
 )
 
-:: Gitからリポジトリをクローン（_tempフォルダに）
-echo Cloning repository...
-git clone "%repo_url%" "%temp_dir%"
-
-:: クローンしたリポジトリから内容をAssetsフォルダにコピー（_tempフォルダの中身のみ）
-echo Copying files...
-xcopy /E /I /Y "%temp_dir%\*" "%target_dir%"
-
-:: 一時フォルダを削除
-echo Cleaning up temporary folder...
-rmdir /S /Q "%temp_dir%"
+:: Gitからリポジトリをクローン
+echo Cloning repository into _develop_shooter folder...
+git clone "%repo_url%" "%target_dir%"
 
 :: 完了メッセージ
-echo Repository has been successfully cloned and copied.
+echo Repository has been successfully cloned into _develop_shooter folder.
 pause
