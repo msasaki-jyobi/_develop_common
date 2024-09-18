97334a1ac06af81473a1fa0ada0b1fd9f166b6b9
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-09-18 15:11:00 +0900

    Update UIStateManager Button Add,Remove

--- Scripts/UI/UIState/UIStateManager.cs
+++ Scripts/UI/UIState/UIStateManager.cs
@@ -16,7 +16,7 @@ namespace develop_common
         [SerializeField] private bool AutoFirstFocus;
         [SerializeField] private bool AutoStartUIState;
         [SerializeField] private string _startUIStateName;
-        
+
         // ステート開いた時に呼び出すイベント
         public event Action<string> ChangeStateEvent;
 
@@ -79,17 +79,69 @@ namespace develop_common
             foreach (var enablebutton in buttons)
                 enablebutton.interactable = true;
             // フォーカスも設定
-            if(buttons != null && buttons.Count != 0)
+            if (buttons != null && buttons.Count != 0)
             {
                 _firstFocusButton = buttons[0];
                 EventSystem.current.SetSelectedGameObject(_firstFocusButton.gameObject);
             }
 
         }
-
+        /// <summary>
+        /// 現在開いているUIステート名を取得
+        /// </summary>
+        /// <returns></returns>
         public string GetCurrentStateName()
         {
             return _currentStateName;
+        }
+
+        /// <summary>
+        /// ボタンをButtonInfosに追加する（Contentへの追加などは別ロジック）
+        /// </summary>
+        /// <param name="interactableStateName">ボタンState名</param>
+        /// <param name="targetButton">追加対象となるボタン</param>
+        /// <param name="checkForDuplicates">重複チェック（重複時は何もせず終了）</param>
+        public void AddInteractableButton(string interactableStateName, Button targetButton, bool checkForDuplicates = false)
+        {
+            List<Button> buttons = new List<Button>();
+
+            for (int i = 0; i < ButtonInfos.Count; i++)
+            {
+                if (ButtonInfos[i].InteractableStateName == interactableStateName) // 一致：ステート名
+                {
+                    buttons = ButtonInfos[i].EnableButtons; // 保持：ボタンリスト
+                    // 重複チェック
+                    if (checkForDuplicates)
+                        foreach (var button in ButtonInfos[i].EnableButtons)
+                            if (button.name == targetButton.name) // 一致：ボタン名
+                                return; // 終了
+
+                }
+            }
+            // ボタン追加
+            buttons.Add(targetButton);
+        }
+
+        public void RemoveInteractableButton(string interactableStateName, string buttonName, bool isSingleRemove = true)
+        {
+            for (int i = 0; i < ButtonInfos.Count; i++)
+            {
+                if (ButtonInfos[i].InteractableStateName == interactableStateName) // 一致：ステート名
+                {
+                    for(int j = 0; j < ButtonInfos[i].EnableButtons.Count;j++) // 検索：ボタンリスト
+                    {
+                        if (ButtonInfos[i].EnableButtons[j].name == buttonName) // 一致：ボタン名
+                        {
+                            var removeButton = ButtonInfos[i].EnableButtons[j];
+                            ButtonInfos[i].EnableButtons.Remove(removeButton); // 削除：リストから
+                            Destroy(removeButton); // 削除：オブジェクト
+
+                            if (isSingleRemove) // 判定：1回のみ
+                                return; // 終了
+                        }
+                    }
+                }
+            }
         }
 
         /// <summary>
