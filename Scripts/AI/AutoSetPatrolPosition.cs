370f6d9bbf3a6c5c502ca7770c27760d044478fa
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-10-09 21:20:37 +0900

    Update　AI強化

---
+++ Scripts/AI/AutoSetPatrolPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace develop_common
+{
+    public class AutoSetPatrolPosition : MonoBehaviour
+    {
+        public NavMeshController NavMeshController;
+        public List<GameObject> PatrolPositions = new List<GameObject>();
+
+        private void Start()
+        {
+            foreach (GameObject obj in PatrolPositions) 
+            {
+                NavMeshController.PatrolPositions.Add(obj);
+            }
+        }
+
+    }
+}
--- Scripts/AI/NavMeshController.cs
+++ Scripts/AI/NavMeshController.cs
@@ -1,5 +1,7 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
@@ -13,61 +15,92 @@ namespace develop_common
         public NavMeshAgent Agent;
         public GameObject Eye;
         public AnimatorStateController AnimatorStateController;
+        public Animator Animator;
 
+        [Header("Patrol Positions")]
+        public List<GameObject> PatrolPositions = new List<GameObject>();
+
+        [Header("AI Speed")]
         public float NormalSpeed = 3.5f;
         public float HuntSpeed = 3.5f;
 
+        [Header("AI Parameter")]
         public bool IsDistance; // 指定距離以内が必要
-        public float distance = 5f;
+        public float Distance = 5f;
         public bool IsLook; // 視認が必要
+        public float RayHeight = 1f; // 視認が必要
+        public bool IsAngle; // 角度が必要
+        public float Angle = 45f; // 角度が必要
+        public float LostHuntTime = 1f; // 見失うまでの時間
 
+        [Header("AI Debug")]
+        public bool IsDebug;
+
+        private float _huntTimer;
         private ReactiveProperty<bool> _isHunt = new ReactiveProperty<bool>();
         private bool _isStop;
+        private GameObject _patrolTarget;
+        private bool _isPatrolCheck;
 
         private void Start()
         {
-            if(Target == null)
+            if (Target == null)
                 Target = GameObject.Find("Player");
 
             // 半径10m以内の徘徊場所を自動取得
 
             _isHunt
-                .Subscribe((x) => 
+                .Subscribe((x) =>
                 {
                     if (_isStop) return;
 
                     if (AnimatorStateController != null)
-                        if(AnimatorStateController.Animator != null)
+                        if (AnimatorStateController.Animator != null)
                         {
                             var motionName = x ? "Run" : "Idle";
                             AnimatorStateController.StatePlay(motionName, EStatePlayType.Loop, false);
                         }
-                
+
                 });
         }
 
         private void Update()
         {
+            if (_huntTimer > 0) _huntTimer -= Time.deltaTime;
+
             if (_isStop) return;
             Move();
+
         }
 
-        private void Move()
+        private async UniTask Move()
         {
             if (Target == null || Agent == null) return;  // Target または Agent がない場合は何もしない
 
             bool check = true;
             if (IsDistance) // 距離をチェック
-                check = check && AIFunction.TargetDistance(Target, gameObject) <= distance;
+                check = check && AIFunction.TargetDistance(Target, gameObject) <= Distance;
             if (IsLook) // 視認をチェック
-                check = check && AIFunction.GetRayTarget(Eye, Target, rayHeight: 0.5f);
+                check = check &&
+                    (AIFunction.GetRayTarget(Eye, Target, rayHeight: RayHeight) == Target ||
+                     AIFunction.GetRayTarget(Eye, Target, rayHeight: RayHeight / 2) == Target ||
+                     AIFunction.GetRayTarget(Eye, Target, rayHeight: RayHeight / 4) == Target);
+            if (IsAngle) // 角度をチェック
+                check = check && Mathf.Abs(AIFunction.TargetAngle(Eye, Target)) <= Angle;
+            if (_huntTimer > 0) // 強制的に見つけている状態にする
+                check = true;
 
+            if (IsDebug)
+                Debug.Log($"Angle:{AIFunction.TargetAngle(Eye, Target)}");
+
+            // ハント状態切替
             _isHunt.Value = check;
             if (check)
             {
                 // ターゲットを追跡するための移動を開始
                 Agent.speed = HuntSpeed;  // 追跡用のスピードに設定
                 Agent.SetDestination(Target.transform.position);  // ターゲットに向かって移動
+                _huntTimer = LostHuntTime;
             }
             else
             {
@@ -75,16 +108,45 @@ namespace develop_common
                 Agent.speed = NormalSpeed;  // 通常の速度に戻す
 
                 // 半径10m以内の探索場所に向かう
-
-
-
+                if (_patrolTarget == null)
+                {
+                    if (PatrolPositions.Count > 0)
+                    {
+                        if(!_isPatrolCheck)
+                        {
+                            _isPatrolCheck = true;
+                            await UniTask.Delay(1000 * Random.Range(0, 5));
+                            _patrolTarget = PatrolPositions[Random.Range(0, PatrolPositions.Count)];
+                            Agent.speed = NormalSpeed;
+                            Agent.SetDestination(_patrolTarget.transform.position);  // ターゲットに向かって移動
+                            _isPatrolCheck = false;
+                        }
+                    }
+                }
+                else
+                {
+                    var distance = Vector3.Distance(transform.position, _patrolTarget.transform.position);
+                    if(distance <= 0.2f)
+                        _patrolTarget = null;
+                }
             }
+
+            if(Animator != null)
+                // 速度をAnimatorに反映する
+                Animator?.SetFloat("Speed", GetAgentSpeed(), 0.1f, Time.deltaTime);
         }
 
         public void OnStopAgent()
         {
             _isStop = true;
             Agent.enabled = false;
+        }
+
+        public float GetAgentSpeed()
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            Vector3 velocity = agent.velocity; // 現在の移動速度のベクトル
+            return velocity.magnitude;  // 移動速度の大きさを取得
         }
     }
 }
