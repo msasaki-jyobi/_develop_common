4723416567c0f3f00636357236b4f82d4ffc23ed
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-10-22 14:26:26 +0900

    Update ExitAdd UnityEventy

--- Scripts/UnityEvent/Collider/EnterUnityEventLoader.cs
+++ Scripts/UnityEvent/Collider/EnterUnityEventLoader.cs
@@ -8,10 +8,11 @@ namespace develop_common
     public class EnterUnityEventLoader : MonoBehaviour
     {
         // タグのリストを指定する
-        public List<string> targetTags = new List<string>() {  "Player", "Unit", "Character"};
+        public List<string> targetTags = new List<string>() { "Player", "Unit", "Character" };
         public bool IsFinishDestroy;
         public float DestroyTime = 0.5f;
         public UnityEvent EnterEvent;
+        public UnityEvent ExitEvent;
 
 
         // 何かに衝突したときの処理
@@ -26,6 +27,16 @@ namespace develop_common
             OnHit(other.gameObject);
         }
 
+        private void OnCollisionExit(Collision collision)
+        {
+            OnExit(collision.gameObject);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnExit(other.gameObject);
+        }
+
         // タグをチェックして削除
         private void OnHit(GameObject hitObject)
         {
@@ -35,6 +46,15 @@ namespace develop_common
                 EnterEvent?.Invoke();
                 if (IsFinishDestroy)
                     Destroy(gameObject, DestroyTime);
+            }
+        }
+
+        private void OnExit(GameObject hit)
+        {
+            // タグのリストに衝突したオブジェクトのタグが含まれているか確認
+            if (targetTags.Contains(hit.tag))
+            {
+                ExitEvent?.Invoke();
             }
         }
     }
