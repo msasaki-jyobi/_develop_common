8b70a5ed0dfc5c6c458b19dba8c4296e51e280d8
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-10-28 06:09:07 +0900

    Update IKSupport

---
+++ Scripts/FastIKSupport/FastIKManager.cs
@@ -0,0 +1,9 @@
+using develop_common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastIKManager : SingletonMonoBehaviour<FastIKManager>
+{
+    public PartAttachment TargetPartAttachment;
+}
