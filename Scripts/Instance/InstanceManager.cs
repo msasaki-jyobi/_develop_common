89f7a748e1d6414fb1fc99645466434ba6aae888
msasaki-jyobi <mao.sasaki@morijyobi.ac.jp>
2024-10-28 07:59:53 +0900

    Update InstanceManager.cs

--- Scripts/Instance/InstanceManager.cs
+++ Scripts/Instance/InstanceManager.cs
@@ -8,5 +8,29 @@ namespace develop_common
     {
         public UnitVoice UnitVoice;
         public UnitActionLoader UnitActionLoader;
+        public UnitShape UnitShape;
+        public List<StringKeyGameObjectValuePair> InstanceBodys = new List<StringKeyGameObjectValuePair>();
+
+        public GameObject SearchObject(string keyName)
+        {
+            foreach(var body in InstanceBodys) 
+            {
+                if (body.Key == keyName)
+                    return body.Value;
+            }
+            return null;
+        }
+        public void CreateObject(string keyName, GameObject prefab, float destroyTime)
+        {
+            foreach (var body in InstanceBodys)
+            {
+                if (body.Key == keyName)
+                {
+                    var ef = GameObject.Instantiate(prefab, body.Value.transform.position, Quaternion.identity);
+                    Destroy(ef, destroyTime);
+
+                }
+            }
+        }
     }
 }
